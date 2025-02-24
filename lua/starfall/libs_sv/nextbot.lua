6a4e452a2e332580cf7b96abf07c4837055cb3c6
GIGABABAIT <zhhristov@abv.bg>
2025-02-24 17:38:32 +0200

    latest shithub

--- lua/starfall/libs_sv/nextbot.lua
+++ lua/starfall/libs_sv/nextbot.lua
@@ -102,9 +102,11 @@ function nextbot_library.create(pos, mdl)
 	nb:SetModel(mdl)
 	nb.instance = instance
 	nb:Spawn()
+
 	if ply ~= SF.Superuser then
 		nb:SetCreator(ply)
 	end
+
 	entList:register(instance, nb)
 
 	if CPPI then nb:CPPISetOwner(ply == SF.Superuser and NULL or ply) end
